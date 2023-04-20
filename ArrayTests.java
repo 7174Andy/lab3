054c7a271007fbc5e20ccdef0c37c744c8616532
7174Andy <hee031011@likelion.org>
2023-04-20 08:45:45 -0700

    Checkpoint

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,29 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
+  @Test
+  public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
+    assertArrayEquals(new int[] { 3 }, input1);
 
+    int[] input2 = { 1, 2, 3, 4, 5 };
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[] { 5, 4, 3, 2, 1 }, input2);
+  }
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {};
+    assertArrayEquals(new int[] {}, ArrayExamples.reversed(input1));
+
+    int[] input2 = { 1, 2, 3, 4, 5 };
+    assertArrayEquals(new int[] { 5, 4, 3, 2, 1 }, ArrayExamples.reversed(input2));
+  }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] input1 = { 1, 2, 3, 4 };
+    assertEquals(3, ArrayExamples.averageWithoutLowest(input1), 0.00001);
   }
 }
