f198ac2e1a52a13c0b0b783ca2d3dce8b7099530
7174Andy <hee031011@likelion.org>
2023-04-21 17:16:29 -0700

    Update

--- ListTests.java
+++ ListTests.java
@@ -7,15 +7,17 @@ public class ListTests {
     @Test
     public void testMerge() {
         ArrayList<String> list1 = new ArrayList<String>(Arrays.asList("a", "b", "c", "d"));
-
         ArrayList<String> list2 = new ArrayList<>();
         list2.add("b");
         list2.add("c");
         list2.add("d");
-
         ArrayList<String> expected = new ArrayList<String>(Arrays.asList("a", "b", "b", "c", "c", "d", "d"));
-
         assertEquals(expected, ListExamples.merge(list1, list2));
+
+        ArrayList<String> list3 = new ArrayList<String>(Arrays.asList("a", "b"));
+        ArrayList<String> list4 = new ArrayList<String>(Arrays.asList("c", "b", "a"));
+        ArrayList<String> expected2 = new ArrayList<String>(Arrays.asList("a", "a", "b", "b", "c"));
+        assertEquals(expected2, ListExamples.merge(list3, list4));
     }
 
     @Test
@@ -26,11 +28,13 @@ public class ListTests {
             }
         };
 
-        ArrayList<String> list = new ArrayList<String>(
+        ArrayList<String> failedList = new ArrayList<String>(
                 Arrays.asList("apple", "beer", "car", "date", "egg", "fish", "grape"));
-
         ArrayList<String> expected = new ArrayList<String>(Arrays.asList("apple", "car", "date", "grape"));
+        assertEquals(expected, ListExamples.filter(failedList, sc));
 
-        assertEquals(expected, ListExamples.filter(list, sc));
+        ArrayList<String> workingList = new ArrayList<String>(Arrays.asList("apple"));
+        ArrayList<String> expected2 = new ArrayList<>(Arrays.asList("apple"));
+        assertEquals(expected2, ListExamples.filter(workingList, sc));
     }
 }
